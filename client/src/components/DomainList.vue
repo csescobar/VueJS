81c06b24f07ec282c706ea21ab61789bd98b2dea
Cesar Escobar <cesarsilvaescobar@gmail.com>
2020-02-27 17:56:30 -0300

    Aula 9

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -46,8 +46,8 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import {mapState, mapActions} from "vuex";
 import AppItemList from "./AppItemList";
-import axios from "axios/dist/axios";
 
 export default {
 	name: "App",
@@ -55,118 +55,18 @@ export default {
 		AppItemList
 	},
 	data: function() {
-		return {
-			items: {
-				prefix: [],
-				sufix: []
-			},
-			domains: []
-		};
+		return {};
 	},
 	methods: {
-		addItem(item) {
-			axios({
-				url: process.env.VUE_APP_URL, //"https://namegator-server.herokuapp.com/", //"http://localhost:3000", //process.env.URL, //"https://namegator-server.herokuapp.com/",
-				method: "post",
-				data: {
-					query: `
-						mutation ($item: ItemInput){
-							newItem: saveItem(item: $item) {
-								id
-								type
-								description
-							}
-						}
-					`,
-					variables: {
-						item
-					}
-				}
-			}).then(response => {
-				const query = response.data;
-				const newItem = query.data.newItem;
-				this.items[item.type].push(newItem);
-				this.generateDomains();
-			}) ;
-			
-		},
-		deleteItem(item) {
-			axios({
-				url: process.env.VUE_APP_URL, //"https://namegator-server.herokuapp.com/", //"http://localhost:3000", //process.env.URL, //"https://namegator-server.herokuapp.com/",
-				method: "post",
-				data: {
-					query: `
-						mutation ($id: Int) {
-							deleteItem(id: $id)
-						}
-					`,
-					variables: {
-						id: item.id
-					}
-				}
-			}).then(() => {
-				this.items[item.type].splice(this.items[item.type].indexOf(item), 1);
-				this.generateDomains();
-			});
-			
-		},
-		getItems(type) {
-			return axios({
-				url: process.env.VUE_APP_URL, //"https://namegator-server.herokuapp.com/", //"http://localhost:3000", //process.env.URL, //
-				method: "post",
-				data: {
-					query: `
-						query ($type: String) { 
-							items: items (type: $type) {
-								id
-								type
-								description
-							}
-						}
-				`,
-					variables: {
-						type
-					}
-				}
-			}).then( response => {
-				const query = response.data;
-				this.items[type] = query.data.items;
-			});
-		},
-		generateDomains() {
-			console.log("generating domains...");
-			axios ({
-				url: process.env.VUE_APP_URL, //"https://namegator-server.herokuapp.com/", //"http://localhost:3000",
-				method: "post",
-				data: {
-					query: `
-						mutation {
-							domains: generateDomains {
-								name
-								checkout
-								available
-							}
-						}
-					`
-				}
-			}).then((response) => {
-				const query = response.data;
-				this.domains = query.data.domains;
-			});
-		},
+		...mapActions(["addItem", "deleteItem", "getItems", "generateDomains"]),
 		openDomain(domain) {
 			this.$router.push({
 				path: `/domains/${domain.name}`
 			})
 		}
 	},
-	created() {
-		Promise.all([
-			this.getItems("prefix"),
-			this.getItems("sufix")
-		]).then(() => {
-			this.generateDomains();
-		});
+	computed: {
+		...mapState(["items", "domains"])
 	}
 };
 </script>
